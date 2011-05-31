7c19bf8359e49fde8a72b44070a35b078226c70e
palewire <ben.welsh@gmail.com>
2011-05-30 17:58:59 -0700

    Prepped for test release

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='python-lametro-api',
+      version='0.1',
+      description='A simple Python wrapper for the L.A. Metro\'s API.',
+      author='Ben Welsh',
+      author_email='ben.welsh@latimes.com',
+      url='http://datadesk.github.com/python-lametro-api/',
+      packages=find_packages(),
+      license='MIT',
+      keywords='losangeles metro transit busses transportation',
+      classifiers=["Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules"
+                   ],
+     )
