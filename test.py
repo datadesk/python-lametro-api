e35336785164e4cc6da24e900946dd456239a589
palewire <ben.welsh@gmail.com>
2014-01-27 22:40:05 -0800

    PEP8 and PyFlakes testing

--- test.py
+++ test.py
@@ -64,7 +64,7 @@ class BusRouteTests(BaseTest):
         """
         Test a request for vehicles on this route.
         """
-        obj = self.client.bus.routes.get(002)
+        obj = self.client.bus.routes.get(2)
         vehicles = obj.vehicles
         self.assertEqual(type(vehicles), type([]))
         self.assertEqual(vehicles[0].longitude, vehicles[0].x)
